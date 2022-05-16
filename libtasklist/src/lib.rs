1c0357c6c40dc3f4360e11308f63dea676e7949a
Gustav Sörnäs <gustav@sornas.net>
2022-05-16 21:18:42 +0200

    get a routine id over http

--- libtasklist/src/lib.rs
+++ libtasklist/src/lib.rs
@@ -1,4 +1,23 @@
+use color_eyre::eyre::Result;
+
 pub mod model;
 
 // B)
 pub type Database = Vec<model::Routine>;
+
+pub fn open() -> Result<Database> {
+    let db_file = std::path::PathBuf::from("routines.json");
+    if db_file.exists() {
+        let content = std::fs::read_to_string(db_file)?;
+        Ok(serde_json::from_str(&content)?)
+    } else {
+        Ok(vec![])
+    }
+}
+
+pub fn store(db: Database) -> Result<()> {
+    let content = serde_json::to_string(&db)?;
+    let db_file = std::path::PathBuf::from("routines.json");
+    std::fs::write(&db_file, &content)?;
+    Ok(())
+}
--- tasklist-server/src/main.rs
+++ tasklist-server/src/main.rs
@@ -1,26 +1,34 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::error::{ErrorBadRequest, ErrorInternalServerError, ErrorNotFound};
+use actix_web::http::header::ContentType;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use tracing::Level;
+use tracing_actix_web::TracingLogger;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[tracing::instrument]
+#[get("/routine/{routine_id}")]
+async fn get_routine(routine_id: web::Path<String>) -> actix_web::Result<impl Responder> {
+    let routine_id: usize = routine_id.into_inner().parse().map_err(ErrorBadRequest)?;
+    let routines = tasklists::open().map_err(ErrorInternalServerError)?;
+    let routine = routines
+        .get(routine_id)
+        .ok_or(ErrorNotFound(format!("Routine {routine_id} not found")))?;
+    let json = serde_json::to_string(&routine).map_err(ErrorInternalServerError)?;
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(json))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .compact()
+        .init();
+
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .wrap(TracingLogger::default())
+            .service(get_routine)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
