59b0d00c2cb9991d529b6c95e88743d056fffbb6
Annie Wång <annie.u.wang@gmail.com>
2022-06-10 22:18:05 +0200

    begin homepage

--- tasklist-webclient/src/App.vue
+++ tasklist-webclient/src/App.vue
@@ -1,30 +1,67 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <n-config-provider :theme="darkTheme">
+    <n-space >
+      <n-layout has-sider style="height: 100vh; display: flex; flex-direction: column;">
+        <n-layout-sider 
+          bordered
+          collapse-mode="width"
+          :collapsed-width="64"
+          :width="240"
+          :collapsed="collapsed"
+          show-trigger
+          @collapse="collapsed = true"
+          @expand="collapsed = false"
+        >
+          <n-menu
+            v-model:value="activeKey"
+            :collapsed="collapsed"
+            :collapsed-width="64"
+            :collapsed-icon-size="22"
+            :options="menuOptions"
+          />
+        </n-layout-sider>
+        <n-layout style="display: flex; flex: 1 0 auto;">
+          <span>
+            <router-view/>
+          </span>
+        </n-layout>
+      </n-layout>
+    </n-space>
+    <n-global-style />
+  </n-config-provider>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import { darkTheme } from 'naive-ui';
+import { h, ref } from 'vue';
+import { NIcon } from 'naive-ui'
+import { BookOutline } from "@vicons/ionicons5"
+
+function renderIcon(icon) {
+  return () => h(NIcon, null, { default: () => h(icon) });
 }
 
-nav {
-  padding: 30px;
-}
+const menuOptions = [
+  {
+    label: "Home",
+    key: "home",
+    icon: renderIcon(BookOutline)
+  },
+  {
+    label: "Not Home",
+    key: "nothome",
+    icon: renderIcon(BookOutline)
+  }
+]
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default {
+  setup() {
+    return {
+      darkTheme,
+      activeKey: ref(null),
+      collapsed: ref(null),
+      menuOptions
+    }
+  }
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
