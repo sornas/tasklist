458a3ff3308f62d70fe82366b4595b02a371f2e3
Gustav Sörnäs <gustav@sornas.net>
2022-06-18 11:10:47 +0200

    refactor function

--- tasklist-lib/src/db/tasklist.rs
+++ tasklist-lib/src/db/tasklist.rs
@@ -6,23 +6,25 @@ use tracing::{event, Level};
 use crate::db;
 use crate::model;
 
-use db::schema;
+use db::schema::{self, tasklist::dsl};
+
+fn tasks_in_tasklist(connection: &SqliteConnection, tasklist: i32) -> Result<Vec<i32>> {
+    use schema::task_partof_regular::dsl;
+    dsl::task_partof_regular
+        .filter(dsl::regular.eq(tasklist))
+        .select(dsl::task)
+        .load(connection)
+        .map_err(ErrorInternalServerError)
+}
 
 #[tracing::instrument(skip(connection))]
 pub fn all_tasklists(connection: &SqliteConnection) -> Result<Vec<model::Tasklist>> {
-    schema::tasklist::dsl::tasklist
+    dsl::tasklist
         .load::<db::model::RegularTasklist>(connection)
         .map_err(ErrorInternalServerError)?
         .iter()
         .map(|tasklist| {
-            let tasks = {
-                use schema::task_partof_regular::dsl;
-                dsl::task_partof_regular
-                    .filter(dsl::regular.eq(tasklist.id))
-                    .select(dsl::task)
-                    .load::<i32>(connection)
-                    .map_err(ErrorInternalServerError)?
-            };
+            let tasks = tasks_in_tasklist(connection, tasklist.id)?;
             tasklist
                 .clone()
                 .to_model(tasks)
@@ -33,7 +35,7 @@ pub fn all_tasklists(connection: &SqliteConnection) -> Result<Vec<model::Tasklis
 
 #[tracing::instrument(skip(connection))]
 pub fn tasklist_by_id(connection: &SqliteConnection, id: i32) -> Result<model::Tasklist> {
-    let tasklist = schema::tasklist::dsl::tasklist
+    let tasklist = dsl::tasklist
         .find(id)
         .first::<db::model::RegularTasklist>(connection)
         .tap(|tasklist| event!(Level::DEBUG, ?tasklist))
@@ -41,13 +43,6 @@ pub fn tasklist_by_id(connection: &SqliteConnection, id: i32) -> Result<model::T
         .map_err(ErrorInternalServerError)?
         .ok_or_else(|| ErrorNotFound(format!("Tasklist {id} not found")))?;
 
-    let tasks = {
-        use schema::task_partof_regular::dsl;
-        dsl::task_partof_regular
-            .filter(dsl::regular.eq(tasklist.id))
-            .select(dsl::task)
-            .load::<i32>(connection)
-            .map_err(ErrorInternalServerError)?
-    };
+    let tasks = tasks_in_tasklist(connection, tasklist.id)?;
     tasklist.to_model(tasks).map_err(ErrorInternalServerError)
 }
